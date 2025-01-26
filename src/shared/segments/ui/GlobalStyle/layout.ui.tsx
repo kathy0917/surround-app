4311f668c09fcd81777ee5455b668a9d5e28728d
Nahyeon Kang <kathy95917@naver.com>
2025-01-26 17:53:09 +0900

    [style] 화면 크기 수정

--- src/shared/segments/ui/GlobalStyle/layout.ui.tsx
+++ src/shared/segments/ui/GlobalStyle/layout.ui.tsx
@@ -5,11 +5,8 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         width: 100%;
-        height: 100%;
+        height: 100vh;
         font-family: 'Arial', sans-serif;
-
-        max-width: 480px; 
-        min-height: 100vh; 
         margin: 0 auto; 
 
         color: #090909; 
